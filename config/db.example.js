6fbb1b995614b0f95cc50f630b2178796401846d
Carlos Cessa <carlos@pixative.com>
2016-06-14 17:01:07 -0500

    Server Startup
    - Added support to start the express server and parse client requests
    - Created database configuration file and utilities library to handle
    the connection with the MongoDB server

---
+++ config/db.example.js
@@ -0,0 +1,7 @@
+module.exports  = {
+    'database'  : '{{MONGO_DB}}',
+    'host'      : '{{MONGO_HOST',
+    'pass'      : '{{MONGO_PASS}}',
+    'port'      : '{{MONGO_PORT}}',
+    'user'      : '{{MONGO_USER}}'
+};
--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 var express         = require( 'express' ),
     app             = express(),
-    debug           = require( 'debug' )( 'app' );
+    debug           = require( 'debug' )( 'app' ),
+    start           = require( './lib/start' );
 
 // Set the PORT and ENV variables and start the server
 app.set( 'port', process.env.PORT || 3000 );
 app.set( 'env', process.env.ENV || 'development' );
+
+start.launch( app );
 
 var server  = app.listen( app.get('port'), function() {
     debug( 'Express server listening on port ' + server.address().port );
