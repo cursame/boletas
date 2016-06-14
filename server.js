a93bb41de8d01e73bf649c1d15081f796e19ca4e
Carlos Cessa <carlos@pixative.com>
2016-06-14 17:51:49 -0500

    Error Handling
    - Added library to handle errors in the server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express         = require( 'express' ),
     app             = express(),
     debug           = require( 'debug' )( 'app' ),
+    error           = require( './lib/error' ),
     start           = require( './lib/start' );
 
 // Set the PORT and ENV variables and start the server
@@ -12,6 +13,9 @@ start.launch( app );
 var sessions        = require( './routers/sessions' );
 
 app.use( '/sessions', sessions );
+
+app.use( error.notFound );
+app.use( error.handler );
 
 var server          = app.listen( app.get('port'), function() {
     debug( 'Express server listening on port ' + server.address().port );
