b75dd3033311abe7484a5d981f89d7cd1f6ce7ad
Carlos Cessa <carlos@pixative.com>
2016-06-23 17:57:45 -0500

    String Helper
    - Created library to encapsulate functionality related to strings
    - Added support to generate random strings
    - Included test describers for the added functionality

--- test/libraries.js
+++ test/libraries.js
@@ -2,11 +2,19 @@ var assert  = require( 'assert' ),
     should  = require( 'should' ),
     request = require( 'supertest' ),
     server  = require( '../server' ),
-    Encrypt = require( '../lib/encrypt' )
+    Encrypt = require( '../lib/encrypt' ),
+    String  = require( '../lib/string' );
 
 describe( 'AnalyticsAPI Libraries', function () {
-    var original    = "HelloWorldUnitTesting",
+    var original    = "",
         encoded     = "";
+
+    it ( 'generate a random string of 12 characters', function ( done ) {
+        original    = String.random( 12, "alnum" );
+
+        assert.equal( original.length, 12 );
+        done();
+    });
 
     it ( 'encode the random string generated', function ( done ) {
         encoded     = Encrypt.encode( original );
@@ -23,6 +31,22 @@ describe( 'AnalyticsAPI Libraries', function () {
 
     it ( 'get an empty string when attempting to decode an invalid cipher', function ( done ) {
         assert.equal( "", Encrypt.decode( "invalid" ) );
+        done();
+    });
+
+    it ( 'generate a random numeric string of 10 characters', function ( done ) {
+        var random  = String.random( 10, 'numeric' );
+
+        assert.equal( random.length, 10 );
+        assert.equal( /^[0-9]+$/.test( random ), true );
+        done();
+    });
+
+    it ( 'generate a random alpha string of 10 characters', function ( done ) {
+        var random  = String.random( 10, 'alpha' );
+
+        assert.equal( random.length, 10 );
+        assert.equal( /^[A-z]+$/.test( random ), true );
         done();
     });
 });
