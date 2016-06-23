59e551b360aa9c19df86f97adccb94a05c40ed1a
Carlos Cessa <carlos@pixative.com>
2016-06-23 17:50:11 -0500

    Sessions Testing
    - Added support to Unit Test the server session's functionality

--- test/server.js
+++ test/server.js
@@ -17,3 +17,50 @@ describe( 'BoletasAPI', function () {
             .expect( 200, done );
     });
 });
+
+describe( 'Sessions', function () {
+    var invalid     = {
+            email   : 'invalid@cursa.me',
+            pass    : 'wrong'
+        },
+        user        = {
+            email   : 'admin@cursa.me',
+            pass    : 'admin'
+        },
+        session     = '';
+
+    it ( 'should get a 403 error when attempting to start a session with invalid credentials', function ( done ) {
+        request( server )
+            .post( '/sessions' )
+            .send( invalid )
+            .expect( 403, done );
+    });
+
+    it ( 'should start a session in the system', function ( done ) {
+        request( server )
+            .post( '/sessions' )
+            .send( user )
+            .end( function ( err, res ) {
+                if ( err ) {
+                    throw err;
+                }
+
+                res.body.should.have.property( 'session' );
+
+                session     = res.body.session;
+                done();
+            });
+    });
+
+    it ( 'should get a 404 error when attempting to delete an unexisting session', function ( done ) {
+        request( server )
+            .delete( '/sessions/4b61cce1e9de32f6c2c77d702b2286ff0058d9011ad69cadb39149f99cafd48b' )
+            .expect( 404, done );
+    });
+
+    it ( 'should terminate the statrted session', function ( done ) {
+        request( server )
+            .delete( '/sessions/' + session )
+            .expect( 200, done );
+    });
+});
